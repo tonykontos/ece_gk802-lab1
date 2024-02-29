7f5a9ab93b901929ded0b2b8f741156f04708a77
tonykontos <antonikontos@gmail.com>
2024-02-29 23:28:51 +0200

    lab1-1

--- lab1.py
+++ lab1.py
@@ -3,34 +3,28 @@
 
 import requests
 
-# Αsk the user to input a URL
-url = input("Please enter the URL: ")
+# Ask the user to input a URL
+url = input('Enter the URL: ')
 
-# Checkin if URL starts with https or http, if not add https:// 
-if not url.startswith("https://") or url.startswith("http://"):
+# Checking if URL starts with https or http, if not add https:// 
+if not url.startswith("https://"):
     url = "https://" + url
 
-# Μake an HTTP request to the URL
-response = requests.get(url)
+# Making an HTTP request to the URL
+with requests.get(url) as response:
+    # Print the HTTP response headers
+    print(f"Website Headers are {url} \n , {response.headers} \n\n")
 
-# Print the HTTP response headers
-print("Response Headers:\n")
-for header in response.headers:
-    print(header + ":", response.headers[header])
+    # Print server software information and cookie information
+    server = response.headers.get('Server')
 
-# Print server software information and cookie information
-server = response.headers.get('server')
-cookies = response.cookies
-if server:
-    print("\nServer is: {server}")
-else:
-    print("\nServer information not available.")
+    if server:
+        print(f"Server is: {server}")
+    else:
+        print("Server information not available.")
 
-if cookies:
-    print("\nCookies:\n")
-    for cookie in cookies:
-        print("Name: {cookie.name}")
-        print("Value: {cookie.value}")
-        print("Expires: {cookie.expires}")
-else:
-    print("\nNo cookies used.")
+    cookies = response.headers.get('Set-Cookie')
+    if cookies:
+        print(f"Cookies are: {cookies} ")
+    else:
+        print("No cookies used.")
